9c0a62441eed81b620180f76e26e1fc6808959f3
Adriano Rodrigues Vieira <adriano.antoniev@outlook.com>
2022-01-16 15:34:28 -0300

    Initial commit

---
+++ ChuchNorrisJokes/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  ChuchNorrisJokes
+//
+//  Created by Adriano Rodrigues Vieira on 16/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
